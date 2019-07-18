4b372e96cfba0b1e2a34d0f40627bb64a71a6213
su <recallsufuture@gmail.com>
2019-07-18 14:35:51 +0800

    用html类库重构homework api

--- lib/api/jxxt/homework.dart
+++ lib/api/jxxt/homework.dart
@@ -2,6 +2,8 @@ import 'package:hblgdx/model/course.dart';
 import 'package:hblgdx/model/homework.dart';
 import 'package:hblgdx/utils/regex.dart';
 import 'package:hblgdx/utils/request.dart';
+import 'package:html/dom.dart';
+import 'package:html/parser.dart';
 
 import 'base.dart';
 
@@ -47,17 +49,12 @@ Future<List<Homework>> getHomeworkList(String courseId) async {
     return null;
   }
 
-  // 获取表格的所有行并遍历
-  List rows = matchAll(r'<tr>((?:.|\n)*?)</tr>', content).toList();
+  var document = parse(content);
+  var trList = document.getElementsByTagName('tr');
   // i=0时获取的是th头信息，需要跳过
-  for (int i = 1; i < rows.length; i++) {
-    Homework homework;
-    String regex = r'<td.*?>((?:.|\n)*?)</td>';
-    String content = rows[i].group(1);
-
-    // 获取表格的所有列并整理成Homework对象
-    homework = Homework.fromMatchList(matchAll(regex, content).toList());
-    result.add(homework);
+  for (int i = 1; i < trList.length; i++) {
+    var tdList = trList[i].getElementsByTagName('td');
+    result.add(_getHomeworkFromTdList(tdList));
   }
 
   return result;
@@ -68,8 +65,8 @@ Future<List<Homework>> getHomeworkList(String courseId) async {
 /// 登录后访问
 Future<String> getHomeworkDetail(String homeworkId) async {
   String regex = r"<input type='hidden'.*?value='(.*?)'>";
-  String content = await request.getContent(
-      '$homeworkDetailUrl?hwtid=$homeworkId');
+  String content =
+  await request.getContent('$homeworkDetailUrl?hwtid=$homeworkId');
   if (content == null) {
     return null;
   }
@@ -85,4 +82,56 @@ Future<String> getHomeworkDetail(String homeworkId) async {
   }
 
   return null;
+}
+
+/// 从td节点列表中获取作业信息
+_getHomeworkFromTdList(List<Element> list) {
+  Homework result = new Homework();
+
+  for (int i = 0; i < list.length; i++) {
+    var td = list[i];
+
+    switch (i) {
+      case 0:
+        result.title = td
+            .querySelector('a')
+            .innerHtml
+            .trim();
+        break;
+      case 1:
+        result.dateTime = td.innerHtml.trim();
+        break;
+      case 2:
+        result.score = td.innerHtml.trim();
+        break;
+      case 3:
+        result.publisher = td.innerHtml.trim();
+        break;
+      case 4:
+        result.countUrl = _getUrlFromTd(td);
+        break;
+      case 5:
+        result.submitUrl = _getUrlFromTd(td);
+        break;
+      case 6:
+        result.resultUrl = _getUrlFromTd(td);
+        break;
+    }
+  }
+
+  result.id = _getHomeworkId(result.countUrl);
+  return result;
+}
+
+/// 从td中获取url
+String _getUrlFromTd(Element td) {
+  var attributes = td
+      .querySelector('a')
+      ?.attributes;
+  return attributes == null ? null : homeworkListUrl + attributes['href'];
+}
+
+/// 从统计信息url中获得作业id
+String _getHomeworkId(String countUrl) {
+  return matchOne(r'hwtid=(\d+)', countUrl)?.group(1);
 }
--- lib/model/homework.dart
+++ lib/model/homework.dart
@@ -1,5 +1,3 @@
-import 'package:hblgdx/utils/regex.dart';
-
 class Homework {
   /// 作业id
   String id;
@@ -8,10 +6,10 @@ class Homework {
   String title;
 
   /// 作业截至日期
-  DateTime dateTime;
+  String dateTime;
 
   /// 作业成绩
-  int score;
+  String score;
 
   /// 作业发布人
   String publisher;
@@ -34,104 +32,5 @@ class Homework {
         'countUrl=$countUrl\n'
         'submitUrl=$submitUrl\n'
         'resultUrl=$resultUrl';
-  }
-
-  /// 从一行数据中获取homework对象
-  static Homework fromMatchList(List<Match> cols) {
-    Homework result = new Homework();
-
-    for (int i = 0; i < cols.length; i++) {
-      String content = cols[i].group(1);
-
-      switch (i) {
-        case 0:
-          result.title = _getHomeworkTitle(content);
-          break;
-        case 1:
-          result.dateTime = _getHomeworkDate(content);
-          break;
-        case 2:
-          result.score = _getHomeworkScore(content);
-          break;
-        case 3:
-          result.publisher = _getHomeworkPublisher(content);
-          break;
-        case 4:
-          result.countUrl = _getHomeworkCount(content);
-          break;
-        case 5:
-          result.submitUrl = _getHomeworkSubmit(content);
-          break;
-        case 6:
-          result.resultUrl = _getHomeworkResult(content);
-          break;
-      }
-    }
-
-    // 获取id
-    result.id = _getHomeworkId(result.countUrl);
-
-    // 返回homework
-    return result;
-  }
-
-  /// 获取作业名
-  static String _getHomeworkTitle(String content) {
-    // 匹配作业名
-    return matchOne(r'<a.*?>(\S+)(?:.|\n)*?</a>', content)?.group(1);
-  }
-
-  /// 获取作业截至日期
-  static DateTime _getHomeworkDate(String content) {
-    // 匹配日期
-    Iterable<Match> ms = matchAll(r'(\d+)', content);
-    var ml = ms.toList();
-    if (ml.length > 0) {
-      int year = int.parse(ml[0].group(1));
-      int month = int.parse(ml[1].group(1));
-      int day = int.parse(ml[2].group(1));
-      return DateTime(year, month, day);
-    } else {
-      return null;
-    }
-  }
-
-  /// 获取作业得分
-  static int _getHomeworkScore(String content) {
-    // 匹配分数
-    Match m = matchOne(r'(\d+)', content);
-    return m != null ? int.parse(m.group(1)) : null;
-  }
-
-  /// 获取发布人姓名
-  static String _getHomeworkPublisher(String content) {
-    // 匹配发布人
-    return matchOne(r'(\S+)', content)?.group(1);
-  }
-
-  /// 获取统计信息链接
-  static String _getHomeworkCount(String content) {
-    // 匹配统计信息
-    Match m = matchOne(r'href="../(.+?)"', content);
-    return m != null ? 'common/hw/${m.group(1)}' : null;
-  }
-
-  /// 获取提交作业按钮的链接
-  static String _getHomeworkSubmit(String content) {
-    // 匹配提交作业
-    Match m = matchOne(r'href="(.+?)"', content);
-    return m != null ? 'common/hw/student/${m.group(1)}' : null;
-  }
-
-  /// 获取查看结果的链接
-  static String _getHomeworkResult(String content) {
-    // 匹配查看结果
-    Match m = matchOne(r'href="(.+?)"', content);
-    return m != null ? 'common/hw/student/${m.group(1)}' : null;
-  }
-
-  /// 从统计信息url中获得作业id
-  static String _getHomeworkId(String countUrl) {
-    return matchOne(r'hwtid=(\d+)', countUrl)?.group(1);
   }
 }
