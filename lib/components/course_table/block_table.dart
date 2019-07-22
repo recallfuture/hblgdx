79ebebdcf702d53c47f33c8199a78f8ef2fc2f85
su <recallsufuture@gmail.com>
2019-07-22 18:57:14 +0800

    完善课表

--- lib/components/course_table/block_table.dart
+++ lib/components/course_table/block_table.dart
@@ -4,41 +4,15 @@ import 'package:hblgdx/components/course_table/week_bar.dart';
 import 'package:hblgdx/components/course_table/white_block.dart';
 import 'package:hblgdx/model/course_table/course.dart';
 
-class BlockTable extends StatelessWidget {
+class BlockTable extends StatefulWidget {
+  @override
+  _BlockTableState createState() => _BlockTableState();
+}
+
+class _BlockTableState extends State<BlockTable> {
   final double blockHeight = 60;
 
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        buildBlocks(),
-        WeekBar(
-          DateTime.now(),
-          color: Colors.white,
-          height: blockHeight,
-        ),
-      ],
-    );
-  }
-
-  /// 左边显示节数的列
-  Widget buildLeftColumn() {
-    return Column(
-      children: List<Widget>.generate(
-        13,
-        (index) => Container(
-          width: 30,
-          height: blockHeight,
-          color: Colors.white,
-          child: Center(
-            child: Text('${index + 1}'),
-          ),
-        ),
-      ),
-    );
-  }
-
-  final List<Course> list = [
+  final List<Course> courses = [
     Course(
       name: '课程名',
       location: '地点',
@@ -68,12 +42,47 @@ class BlockTable extends StatelessWidget {
     ),
   ];
 
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        buildBlocks(),
+        buildTopBar(),
+      ],
+    );
+  }
+
+  Widget buildTopBar() {
+    return WeekBar(
+      DateTime.now(),
+      color: Colors.white,
+      height: blockHeight,
+    );
+  }
+
+  /// 左边显示节数的列
+  Widget buildLeftColumn() {
+    return Column(
+      children: List<Widget>.generate(
+        13,
+        (index) => Container(
+          width: 30,
+          height: blockHeight,
+          color: Colors.white,
+          child: Center(
+            child: Text('${index + 1}'),
+          ),
+        ),
+      ),
+    );
+  }
+
   /// 中间的所有列
   List<Widget> buildMainColumns() {
     // 一周7天
     return List<Widget>.generate(7, (index) {
       // 获取当天的课
-      Iterable<Course> day = list.where((c) => c.weekDay == index + 1);
+      Iterable<Course> day = courses.where((c) => c.weekDay == index + 1);
       List<Widget> cols = new List();
 
       // 遍历每天的13节课
@@ -93,11 +102,12 @@ class BlockTable extends StatelessWidget {
         if (jie >= 0 && jie < 13) {
           cols.add(
             CourseBlock(
-              '${course.name}@${course.teacher}',
+              course,
               size: jie + 1,
               height: blockHeight,
               backgroundColor: Color.fromARGB(255, 250, 107, 91),
               textColor: Colors.white,
+              onTap: () => onTap(course),
             ),
           );
           i += jie;
@@ -124,6 +134,50 @@ class BlockTable extends StatelessWidget {
           ]..addAll(buildMainColumns()),
         ),
       ),
+    );
+  }
+
+  onTap(Course course) {
+    showDialog(
+      context: context,
+      builder: (ctx) {
+        return SimpleDialog(
+          title: Text(
+            '查看',
+            textAlign: TextAlign.center,
+          ),
+          titlePadding: EdgeInsets.all(10),
+          elevation: 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(6),
+            ),
+          ),
+          children: <Widget>[
+            ListTile(
+              leading: Text("课程名："),
+              title: Text(course.name),
+              onTap: () {
+                setState(() {
+                  course.name = '123';
+                });
+              },
+            ),
+            ListTile(
+              leading: Text("时间："),
+              title: Text('星期${course.weekDay}'),
+            ),
+            ListTile(
+              leading: Text("地点："),
+              title: Text(course.location),
+            ),
+            ListTile(
+              leading: Text("教师："),
+              title: Text(course.teacher),
+            ),
+          ],
+        );
+      },
     );
   }
 }
--- lib/components/course_table/course_block.dart
+++ lib/components/course_table/course_block.dart
@@ -1,31 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:hblgdx/model/course_table/course.dart';
 
 class CourseBlock extends StatelessWidget {
-  final String title;
+  final Course course;
   final Color backgroundColor;
   final Color textColor;
   final int size;
   final double height;
+  final onTap;
 
-  CourseBlock(this.title,
-      {this.backgroundColor = Colors.black12,
-      this.textColor = Colors.grey,
-      this.size = 1,
-      this.height = 60});
+  CourseBlock(this.course, {
+    this.backgroundColor = Colors.black12,
+    this.textColor = Colors.grey,
+    this.size = 1,
+    this.height = 60,
+    this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: height * size,
-      margin: EdgeInsets.all(1),
-      padding: EdgeInsets.all(1),
-      decoration: BoxDecoration(
-        color: backgroundColor,
-        borderRadius: BorderRadius.all(Radius.circular(5)),
-      ),
-      child: Text(
-        title,
-        style: TextStyle(color: textColor),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: height * size,
+        margin: EdgeInsets.all(1),
+        padding: EdgeInsets.all(1),
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+        ),
+        child: Text(
+          '${course.name}@${course.teacher}',
+          style: TextStyle(color: textColor),
+        ),
       ),
     );
   }
