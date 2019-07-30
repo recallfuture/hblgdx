24ac9a6d571b94abc0cf41a3e651055685a83bb3
su <recallsufuture@gmail.com>
2019-07-30 10:41:42 +0800

    改为从码云检查更新，对国内用户更友好

--- lib/api/github/update.dart
+++ lib/api/github/update.dart
@@ -3,8 +3,7 @@ import 'dart:convert';
 import 'package:hblgdx/model/version.dart';
 import 'package:hblgdx/utils/request.dart';
 
-String versionUrl =
-    'https://raw.githubusercontent.com/recallfuture/hblgdx/master/version';
+String versionUrl = 'https://gitee.com/recallfuture/hblgdx/raw/master/version';
 
 Future<Version> getLatestVersion() async {
   Response response = await request.get(versionUrl);
